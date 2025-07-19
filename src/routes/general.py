c6c4471f83e41249ed0661b195f339d3d0ebf1e3
DanielJerrehian <danieljerrehian@gmail.com>
2025-07-19 19:57:43 +0200

    Move cron-job to task endpoint

--- src/routes/general.py
+++ src/routes/general.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 
 from src.models.user import User
+from src.models.general import Task
 from src.utils.auth import verify
+from src.tasks.main import main
 
 router = APIRouter()
 
@@ -14,3 +16,10 @@ async def root():
 async def meetings(user: User = Depends(verify)):
     if "Meetings" not in user.roles:
         raise HTTPException(status_code=403, detail=f"User with E-Mail '{user.email}' does not have Meetings access")
+    
+@router.post("/task")
+async def task(data: Task):
+    if data.password != "cron-job":
+        return HTTPException(status_code=403, detail="Access prohibited")
+    main()
+    return {"message": "Task(s) completed successfully"}
